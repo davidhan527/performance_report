0d1acfd2952e5bd0e1afcf2791b89c90bf63bc0e
davidhan527 <davidhan527@gmail.com>
2013-12-04 17:09:06 -0500

    cleaning

--- app/controllers/advertisers_controller.rb
+++ app/controllers/advertisers_controller.rb
@@ -1,6 +1,5 @@
 class AdvertisersController < ApplicationController
   def index
-    @advertisers = Advertiser.order("date asc")
     @advertisers =  Advertiser.pluck(:advertiser_name).uniq
   end
 
